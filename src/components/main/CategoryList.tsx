9abfc0448a75f404ba360da2656b28416e7ac285
hanumoka <amagramer@gmail.com>
2022-10-29 14:44:44 +0900

    반응형 디자인 적용

--- src/components/main/CategoryList.tsx
+++ src/components/main/CategoryList.tsx
@@ -19,27 +19,37 @@ type CategoryItemProps = {
     to: string;
   } & CategoryItemProps
 
-const CategoryListWrapper = styled.div`
+  const CategoryListWrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
   width: 768px;
   margin: 100px auto 0;
+
+  @media (max-width: 768px) {
+    width: 100%;
+    margin-top: 50px;
+    padding: 0 20px;
+  }
 `
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 const CategoryItem = styled(({ active, ...props }: GatsbyLinkProps) => (
-    <Link {...props} />
-  ))<CategoryItemProps>`
-    margin-right: 20px;
-    padding: 5px 0;
-    font-size: 18px;
-    font-weight: ${({ active }) => (active ? '800' : '400')};
-    cursor: pointer;
-  
-    &:last-of-type {
-      margin-right: 0;
-    }
-  `
+  <Link {...props} />
+))<CategoryItemProps>`
+  margin-right: 20px;
+  padding: 5px 0;
+  font-size: 18px;
+  font-weight: ${({ active }) => (active ? '800' : '400')};
+  cursor: pointer;
+
+  &:last-of-type {
+    margin-right: 0;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 15px;
+  }
+`
 
 
 const CategoryList: FunctionComponent<CategoryListProps> = function ({
