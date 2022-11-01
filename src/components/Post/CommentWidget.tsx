bd959cc9bf994608d7aca818066055d3d5af9b32
hanumoka <amagramer@gmail.com>
2022-11-01 21:28:28 +0900

    Utterances 설정

--- src/components/Post/CommentWidget.tsx
+++ src/components/Post/CommentWidget.tsx
@@ -1,7 +1,7 @@
 import React, { createRef, FunctionComponent, useEffect } from 'react'
 
 const src = 'https://utteranc.es/client.js'
-const repo = '[username]/[username].github.io' // 자신 계정의 레포지토리로 설정
+const repo = 'hanumoka/hanumoka.github.io/blog' // 자신 계정의 레포지토리로 설정
 
 type UtterancesAttributesType = {
   src: string
