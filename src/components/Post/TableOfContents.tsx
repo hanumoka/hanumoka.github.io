b6e4cee96f655bf9a830fb2a5238f25655e67dfc
hanumoka <amagramer@gmail.com>
2022-12-11 17:30:58 +0900

    포스트 상세 화면에 toc 버튼 추가

--- src/components/Post/TableOfContents.tsx
+++ src/components/Post/TableOfContents.tsx
@@ -1,17 +1,40 @@
-// @ts-nocheck // TODO: 나중에 타입스크립트 
-import React , {FunctionComponent} from 'react'
+// @ts-nocheck // TODO: 나중에 타입스크립트
+import React, { FunctionComponent } from 'react';
 
-const TableOfContents: FunctionComponent = function ({toc}) {
-    return (
-        <div className="toc-container">
-          <div className="toc-wrapper">
-            <div className="toc-content">
-              <div className="toc" dangerouslySetInnerHTML={{ __html: toc }} />
-            </div>
-            {/* <div className="toc-open-btn" onClick={onClickTOCOpen}></div> */}
-          </div>
-        </div>
-      )
-}
+const TableOfContents: FunctionComponent = function ({ toc }) {
+  const onClickToc = () => {
+    alert('클릭');
+  };
+
+  return (
+    <div>
+      <div class="fixed right-0 top-0 py-2 px-14">
+        <button
+          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
+          onClick={onClickToc}
+        >
+          TOC 버튼
+        </button>
+      </div>
+      <div>
+        {/* <div>
+              <div dangerouslySetInnerHTML={{ __html: toc }} />
+            </div> */}
+        {/* <div className="toc-open-btn" onClick={onClickTOCOpen}></div> */}
+      </div>
+      {/* <div class="fixed bottom-0 w-full">
+        <button class="animate-pulse my-8 float-right px-5 py-2 bg-red-500 text-white text-sm font-bold tracking-wide rounded-full focus:outline-none">
+          Back
+        </button>
+        <button class="animate-bounce my-8 ml-auto px-5 py-2 bg-red-500 text-white text-sm font-bold tracking-wide rounded-full focus:outline-none">
+          Next(Quiz)
+        </button>
+        <button class="bottom-0 my-8 float-right px-5 py-2 bg-red-500 text-white text-sm font-bold tracking-wide rounded-full focus:outline-none">
+          Next
+        </button>
+      </div> */}
+    </div>
+  );
+};
 
 export default TableOfContents;
