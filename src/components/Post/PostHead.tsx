2ae30d1749856c673a0588da9b0d48a97ca98c20
hanumoka <amagramer@gmail.com>
2023-01-07 20:19:51 +0900

    1

--- src/components/Post/PostHead.tsx
+++ src/components/Post/PostHead.tsx
@@ -1,23 +1,22 @@
-import React, { FunctionComponent } from 'react'
-import styled from '@emotion/styled'
-import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image'
+import React, { FunctionComponent } from 'react';
+import styled from '@emotion/styled';
+import { GatsbyImage, IGatsbyImageData } from 'gatsby-plugin-image';
 
-import PostHeadInfo, { PostHeadInfoProps } from 'components/Post/PostHeadInfo'
+import PostHeadInfo, { PostHeadInfoProps } from 'components/Post/PostHeadInfo';
 
 type PostHeadProps = PostHeadInfoProps & {
   thumbnail: {
     childImageSharp: {
-      gatsbyImageData: IGatsbyImageData
-    }
-  }
-}
+      gatsbyImageData: IGatsbyImageData;
+    };
+  };
+};
 
 type GatsbyImgProps = {
-  image: IGatsbyImageData
-  alt: string
-  className?: string
-}
-
+  image: IGatsbyImageData;
+  alt: string;
+  className?: string;
+};
 
 const PostHeadWrapper = styled.div`
   position: relative;
@@ -25,41 +24,38 @@ const PostHeadWrapper = styled.div`
   height: 400px;
 
   @media (max-width: 768px) {
-    height: 300px;
+    height: 320px;
   }
-`
+`;
 
-const BackgroundImage = styled((props: GatsbyImgProps) => (
-  <GatsbyImage {...props} style={{ position: 'absolute' }} />
-))`
+const BackgroundImage = styled((props: GatsbyImgProps) => <GatsbyImage {...props} style={{ position: 'absolute' }} />)`
   z-index: -1;
   width: 100%;
-  height: 300px;
+  height: 320px;
   object-fit: cover;
-  filter: brightness(0.25);
+  filter: brightness(0.7);
 
   @media (max-width: 768px) {
-    height: 300px;
+    height: 320px;
   }
-`
+`;
 
 const PostHead: FunctionComponent<PostHeadProps> = function ({
-    title,
-    date,
-    categories,
-    thumbnail: {
-      childImageSharp: { gatsbyImageData },
-    },
-  }) {
+  title,
+  date,
+  categories,
+  thumbnail: {
+    childImageSharp: { gatsbyImageData },
+  },
+}) {
+  return (
+    <PostHeadWrapper>
+      <div className="dark:bg-slate-900 dark:h-80">
+        <BackgroundImage image={gatsbyImageData} alt="thumbnail" />
+        <PostHeadInfo title={title} date={date} categories={categories} />
+      </div>
+    </PostHeadWrapper>
+  );
+};
 
-    return (
-      <PostHeadWrapper>
-        <div>
-          <BackgroundImage image={gatsbyImageData} alt="thumbnail" />
-          <PostHeadInfo title={title} date={date} categories={categories} />
-        </div>
-      </PostHeadWrapper>
-    )
-  }
-  
-  export default PostHead
+export default PostHead;
