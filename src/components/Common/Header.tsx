6f9040960bb37bce339e53b6672522058a0ccb38
hanumoka <amagramer@gmail.com>
2023-01-07 20:24:16 +0900

    1

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -2,6 +2,7 @@
 import React, { FunctionComponent } from 'react';
 import styled from '@emotion/styled';
 import ProfileImage from 'components/Main/ProfileImage';
+import { Link } from 'gatsby';
 import { IGatsbyImageData } from 'gatsby-plugin-image';
 import Navbar from './Navbar';
 
@@ -54,7 +55,14 @@ const Header: FunctionComponent<HeaderProps> = function ({ profileImage }) {
           </div>
           <div className="flex-1 w-74">
             <SubTitle>Hanumoka DEV Blog</SubTitle>
-            <Title>CodingBluse</Title>
+            <Title>
+              <Link
+                className="text-xl block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                to="/"
+              >
+                CodingBlues
+              </Link>
+            </Title>
           </div>
           <div className="flex-2 w-22">
             <Navbar />
