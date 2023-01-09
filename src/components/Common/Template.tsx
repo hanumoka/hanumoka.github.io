d6373316a1609a880bb20a2cdc67962fdfce5381
hanumoka <amagramer@gmail.com>
2023-01-09 21:15:50 +0900

    google search console 메타 테그

--- src/components/Common/Template.tsx
+++ src/components/Common/Template.tsx
@@ -44,20 +44,20 @@ const Template: FunctionComponent<TemplateProps> = function ({
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Type" content="text/html;charset=UTF-8" />
 
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={title} />
-        <meta property="og:description" content={description} />
-        <meta property="og:image" content={image} />
-        <meta property="og:url" content={url} />
-        <meta property="og:site_name" content={title} />
+        {/* <meta property="og:type" content="website" /> */}
+        {/* <meta property="og:title" content={title} /> */}
+        {/* <meta property="og:description" content={description} /> */}
+        {/* <meta property="og:image" content={image} /> */}
+        {/* <meta property="og:url" content={url} /> */}
+        {/* <meta property="og:site_name" content={title} /> */}
 
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:title" content={title} />
-        <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={image} />
+        {/* <meta name="twitter:card" content="summary" /> */}
+        {/* <meta name="twitter:title" content={title} /> */}
+        {/* <meta name="twitter:description" content={description} /> */}
+        {/* <meta name="twitter:image" content={image} /> */}
         {/* TODO: twitter 해당 내용 확인하기 */}
-        <meta name="twitter:site" content="@사용자이름" />
-        <meta name="twitter:creator" content="@사용자이름" />
+        {/* <meta name="twitter:site" content="@사용자이름" /> */}
+        {/* <meta name="twitter:creator" content="@사용자이름" /> */}
 
         <meta name="google-site-verification" content="VMkP0_r8pL9_xczIsGhPaHsuo55RNiskxagZq4gYP9c" />
 
